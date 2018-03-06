8ced0be3e9ee07cb3b9b005f342bacc27a78a064
Phoebe Wooldridge <pwooldri@minint-r9hb80h.corporate.thoughtworks.com>
2018-03-06 14:45:03 -0600

    adding new class for prime factor assignment

---
+++ TWU101_Intro_Exercises/src/PrimeFactors.java
@@ -0,0 +1,12 @@
+public class PrimeFactors {
+
+    int[] generate(int n){
+        int[] setup = new int[10];
+        return setup;
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
